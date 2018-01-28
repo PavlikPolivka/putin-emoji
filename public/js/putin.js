008a987e7eb6a7be482f0a43cf71ff99ea2c5b0c
Pavel Polivka <polivka.pavel@gmail.com>
2018-01-28 14:40:23 +0100

    ga

--- public/js/putin.js
+++ public/js/putin.js
@@ -13,8 +13,18 @@ $(document).ready(function () {
                     $("#imageBox").html("<img id='putin' src='"+ imagePath + "'/>");
                     $("#linkBox").val("https://putinemoji.com" + imagePath);
                     $("#resultBox").show();
+                    gtag({
+                        'event':'VirtualPageview',
+                        'virtualPageURL':'/search?searchText=' + str,
+                        'virtualPageTitle' : str
+                    });
+                } else {
+                    gtag({
+                        'event':'VirtualPageview',
+                        'virtualPageURL':'/search?searchText=' + str,
+                        'virtualPageTitle' : "404"
+                    });
                 }
-                ga('send', 'pageview', '/search?searchText=' + str);
             });
     }
 
